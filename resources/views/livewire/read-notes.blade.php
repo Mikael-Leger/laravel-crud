f24946d5623261414ad43ebf2ffe7541d824a308
Mikael-Leger <mikael.leger8@gmail.com>
2025-03-06 19:09:43 +0100

    style/table-is-now-responsive

--- resources/views/livewire/read-notes.blade.php
+++ resources/views/livewire/read-notes.blade.php
@@ -8,8 +8,8 @@
         </form>
     </div>
 
-    <div class="w-full border-1-solid-black br-5 overflow-hidden mt-2">
-        <div class="table-row bg-blue text-white font-bold p-2">
+    <div class="w-full border-1-solid-black br-5 overflow-scroll mt-2">
+        <div class="table-header table-row bg-blue text-white font-bold p-2">
             <span class="flex-centered">Id</span>
             <span class="flex-centered">Title</span>
             <span class="flex-centered">Slug</span>
@@ -19,32 +19,34 @@
             <span class="flex-centered">Actions</span>
         </div>
 
-        @foreach ($notes as $index => $note)
-            <div class="table-row p-2 border-b border-gray-300 
-                        {{ $index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200' }}">
-                <div class="contents">
-                    <span class="flex-centered">{{ $note->id }}</span>
-                    <span class="flex-centered">{{ $note->title }}</span>
-                    <span class="flex-centered">{{ $note->slug }}</span>
-                    <span class="flex-centered truncate">{{ $note->content }}</span>
-                    <span class="flex-centered">@livewire('utils-date-format', ['date' => $note->deadline])</span>
-                    <span class="flex-centered">{{ $note->is_done ? 'Finished' : 'Not finished' }}</span>
-                    <span class="flex-centered">
-                        <x-button onclick="window.location.href='{{ route('notes.read', $note->id) }}'" color="purple">Read</x-button>
-                        <x-button onclick="window.location.href='{{ route('notes.edit', $note->id) }}'" color="orange">Edit</x-button>
-                        @livewire('delete-note', ['noteId' => $note->id])
-                    </span>
+        <div class="table-content">
+            @foreach ($notes as $index => $note)
+                <div class="table-row p-2 border-b border-gray-300 
+                            {{ $index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200' }}">
+                    <div class="contents">
+                        <span class="flex-centered">{{ $note->id }}</span>
+                        <span class="flex-centered text-ellipsis break-words">{{ $note->title }}</span>
+                        <span class="flex-centered text-ellipsis break-words">{{ $note->slug }}</span>
+                        <span class="flex-centered text-ellipsis break-words">{{ $note->content }}</span>
+                        <span class="flex-centered text-ellipsis break-words">@livewire('utils-date-format', ['date' => $note->deadline])</span>
+                        <span class="flex-centered text-ellipsis break-words">{{ $note->is_done ? 'Finished' : 'Not finished' }}</span>
+                        <span class="flex-centered table-actions">
+                            <x-button onclick="window.location.href='{{ route('notes.read', $note->id) }}'" color="purple">Read</x-button>
+                            <x-button onclick="window.location.href='{{ route('notes.edit', $note->id) }}'" color="orange">Edit</x-button>
+                            @livewire('delete-note', ['noteId' => $note->id])
+                        </span>
+                    </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
 
-        @if($notes->count() === 0)
-            <div class="table-row p-2 border-b border-gray-300 bg-gray-100">
-                <div class="contents">
-                    <span class="flex-centered col-span-7">No record. Please create your first Note below.</span>
+            @if($notes->count() === 0)
+                <div class="table-row p-2 border-b border-gray-300 bg-gray-100">
+                    <div class="contents">
+                        <span class="flex-centered col-span-7">No record. Please create your first Note below.</span>
+                    </div>
                 </div>
-            </div>
-        @endif
+            @endif
+        </div>
     </div>
 
     <x-button onclick="window.location.href='{{ route('notes.create')}}'" color="blue">Create</x-button>
